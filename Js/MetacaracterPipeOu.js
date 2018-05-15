490b475a17975bb7976a65cc83d86c0af9e05203
Osmar <osmargv99@gmail.com>
2018-05-14 23:06:59 -0300

    car

--- Js/MetacaracterPipeOu.js
+++
@@ -1,5 +0,0 @@
-const texto = 'voce precisa responder sim, não ou não sei'
-
-const regex = /sim|não|sei/g
-
-console.log(texto.match(regex));
--- Js/MetacaracteresScape.js
+++
@@ -1,7 +0,0 @@
-const texto = '1,2,3,4,5,6,7,8,9,a,b,c!d?e'
-const regexPonto = /\./g 
-//   \ -> scape torna o metacaracter literal 
-const regexSimbolo = /,|\.|\?|!| /g
-// dara split em todos de forma global 
-console.log(texto.split(regexSimbolo));
-
